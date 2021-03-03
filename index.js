2bb811077fb801a82a464e8b685e85214370905f
nachovazquez@msn.com <nachovazquez@msn.com>
2021-03-03 13:32:32 +0100

    me he fumado un porro

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+const { createSecureContext } = require("tls");
 
 function generarResultado() {
   const result = {};
@@ -17,8 +18,10 @@ function generarResultado() {
     })
     parseCallesToJson(lines, result);
     parseCochesToJson(lines, result);
-    callesMasUsadas(result);
-    console.log(result);
+    parseCrucesToJson(result);
+    // callesMasUsadas(result);
+    // console.log(result);
+    simulacion1(result);
   });
 }
 
@@ -34,7 +37,9 @@ const parseCallesToJson = (data, result) => {
         finalDeCalle: calleData[1],
         nombreCalle: calleData[2],
         tiempoDeViaje: calleData[3],
-        score: 0,
+        cochesEnElSemaforo: [],
+        cochesEnElRecorrido: [],
+        luzVerde: false
       })
     }
   })
@@ -47,11 +52,11 @@ const parseCochesToJson = (data, result) => {
   data.forEach((calle, i) => {
     if (i >= numeroCalles + 1) {
       const calleData = calle.split(' ');
-      const numeroDeCallesRecorridas = calleData[0];
+      const callesPorRecorrer = calleData[0];
 
       let tramos = [];
       calleData.forEach((tramo, i) => {
-        if (i > 0 && i <= numeroDeCallesRecorridas) {
+        if (i > 0 && i <= callesPorRecorrer) {
           tramos.push(tramo);
         }
       })
@@ -66,34 +71,148 @@ const parseCochesToJson = (data, result) => {
       });
 
       result.coches.push({
-        numeroDeCallesRecorridas: calleData[0],
+        callesPorRecorrer: calleData[0],
         ruta: tramos,
         tiempoMinimo: tiempoMinimo,
+        posicionEnLaRuta: 0,
+        tiempoRecorridoEnLaCalle: 0
       })
     }
   })
 }
-const callesMasUsadas = (result) => {
-  let callesUsadas = [];
-  result.coches.forEach((coche) => {
-    coche.ruta.forEach((calle) => {
-      callesUsadas.push(calle);
+
+const parseCrucesToJson = (result) => {
+  let cruces = [];
+  result.cruces = []
+
+  result.calles.forEach(calle => {
+    cruces.push({
+      id: calle.finalDeCalle,
+      callesEntrantes: [calle.nombreCalle],
+      calleConMasCoches: {
+        calle: "",
+        cochesEnEstaCalle: 0
+      }
+    })
+  });
+
+  result.calles.forEach(calle => {
+    cruces.forEach(cruce => {
+      if (cruce.id === calle.finalDeCalle) {
+        if (cruce.callesEntrantes.includes(calle.nombreCalle) === false) {
+          cruce.callesEntrantes.push(calle.nombreCalle);
+        }
+      }
     });
   });
-  result.calles.forEach(calle => {
-    let score = callesUsadas.filter(el => el === calle.nombreCalle);
-    score = score.length;
-    calle.score = score;
-    if (score < 10) {
-      return;
-    }
+
+  var hash = {};
+  cruces = cruces.filter(function (current) {
+    var exists = !hash[current.id];
+    hash[current.id] = true;
+    return exists;
+  });
+
+  cruces.forEach(cruce => {
+    result.cruces.push(cruce);
   });
 }
 
+// const callesMasUsadas = (result) => {
+//   let callesUsadas = [];
+//   result.coches.forEach((coche) => {
+//     coche.ruta.forEach((calle) => {
+//       callesUsadas.push(calle);
+//     });
+//   });
+//   result.calles.forEach(calle => {
+//     let score = callesUsadas.filter(el => el === calle.nombreCalle);
+//     score = score.length;
+//     calle.score = score;
+//     if (score < 10) {
+//       return;
+//     }
+//   });
+// }
+
 const simulacion1 = (result) => {
-  // recoger tiempo 
-  // iterar sobre el tiempo
-  // en cada segundo, medir cuantos coches hay en el final de cada calle
-  // poner en verde {$coches} segundos el que más tenga
+  let score = 0;
+  let callesConMasCoches = [];
+  let output = [];
+
+  // llenar un array con los coches que estan en el semaforo de cada calle
+  result.calles.forEach(calle => {
+    result.coches.forEach(coche => {
+      if (calle.nombreCalle === coche.ruta[0]) {
+        calle.cochesEnElSemaforo.push(coche);
+      }
+    });
+  });
+  // hacer una serie de cosas cada segundo de la simulacion 
+  for (tiempo = 0; tiempo < result.duracion; tiempo++) {
+    // contar los coches que hay en cada calle
+    result.calles.forEach(calle => {
+      let cochesEnElSemaforo = calle.cochesEnElSemaforo.length;
+      let cochesEnElRecorrido = calle.cochesEnElRecorrido.length;
+      let cochesEnEstaCalle = cochesEnElSemaforo + cochesEnElRecorrido;
+      callesConMasCoches.push({
+        calle: calle.nombreCalle,
+        cochesEnEstaCalle: cochesEnEstaCalle
+      })
+    })
+    // abrir las calles que mas coches tengan de cada interseccion si no estan ya abiertos y cerrar el que no
+    result.cruces.forEach(cruce => {
+      callesConMasCoches.forEach(calleConMasCoches => {
+        result.calles.forEach(calle => {
+          if (cruce.calleConMasCoches.cochesEnEstaCalle < calleConMasCoches.cochesEnEstaCalle && cruce.callesEntrantes.includes(calleConMasCoches.calle) === true) {
+            cruce.calleConMasCoches = calleConMasCoches
+          }
+          if (cruce.calleConMasCoches.calle === calle.nombreCalle) {
+            calle.luzVerde = true;
+          }
+          if (cruce.calleConMasCoches.calle !== calle.nombreCalle) {
+            calle.luzVerde = false;
+          }
+        });
+      });
+    });
+    // registrar en el output las calles que abres nuevas y sumar 1 segundo a las calle que ya estan abiertas
+    // result.cruces.forEach(cruce => {
+    //   if (cruce.calleConMasCoches.calle !== "") {
+    //     output.push({
+    //       cruce: cruce.id,
+    //       calle: cruce.calleConMasCoches.calle,
+    //       tiempoAbierto: 1
+    //     })
+    //   }
+    // });
+    // tener en cuenta que solo se puede nombrar una calle una vez y que se va repetir el ciclo de lo que esté apuntado en cada interseccion
+
+    // mover los coches =>
+    // sumar 1 al tiempo de recorrido de los coches que esten recorriendo la calle
+    result.calles.forEach(calle => {
+      calle.cochesEnElRecorrido.forEach(cocheEnElRecorrido => {
+        cocheEnElRecorrido.tiempoRecorridoEnLaCalle = cocheEnElRecorrido.tiempoRecorridoEnLaCalle + 1;
+      });
+    });
+    // meter los coches que acaban de salir en el array de los que estan recorriendo la calle a la que entran
+    // quitar el primero de la cola de las calles con el semaforo abierto
+    // sumar 1 a la posicion en la ruta de los coches entran a una nueva calle
+    result.calles.forEach(calle => {
+      result.coches.forEach(coche => {
+        if (calle.luzVerde = true && calle.cochesEnElSemaforo[0] == coche) {
+          coche.posicionEnLaRuta = coche.posicionEnLaRuta + 1;
+          calle.cochesEnElSemaforo.splice(0, 1)
+        }
+        if (calle.nombreCalle == coche.ruta[coche.posicionEnLaRuta] && calle.cochesEnElRecorrido.includes(coche) == false) {
+          calle.cochesEnElRecorrido.push(coche);
+        }
+      });
+    });
+    // a los coches que ya han llegado a los segundos que se tarda en recorrer esa calle, meterlos en la cola del semaforo 
+    // borrar los coches que hayan llegado al ultimo punto de la ruta 
+    // sumar el score de esos coches (numero de coches que han llegado x(puntosExtra+(duracion-tiempo))) 
+  }
 }
 generarResultado();
+
